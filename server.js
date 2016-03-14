5f108e3cb3457979f0c75bc53d7091129bb3bf32
Cornerbodega <merhone@gmail.com>
2016-03-13 22:43:46 -0700

    where do I go?

--- server.js
+++ server.js
@@ -244,7 +244,7 @@ db.once('open', function() {
 //     });
 // }
 
-export NODE_ENV=production
+// export NODE_ENV=production
 var port = process.env.PORT || 5000;
 http.listen(port, function() {
     console.log("Listening on " + port);
